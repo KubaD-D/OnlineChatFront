2877b6f0856bcb744be6e600f9daf0a9029b5ee7
KubaDD <kuburolek11@gmail.com>
2024-02-09 00:53:55 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import ChatRoom from './pages/ChatRoom';
 
 function App() {
   return (
-    <h1>Online chat</h1>
+    <>
+      <h1>Online chat</h1>
+      <ChatRoom id={10}/>
+    </>
   );
 }
 
