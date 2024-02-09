e3310050f755094bd34d7dfe051b7fe4c96e607f
KubaDD <kuburolek11@gmail.com>
2024-02-09 15:04:03 +0100

    Added ChatRoom prototype and React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import ChatRoom from './pages/ChatRoom';
+import NavBar from './components/NavBar';
+import Home from './pages/Home';
 
 function App() {
   return (
     <>
-      <h1>Online chat</h1>
-      <ChatRoom id={10}/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/ChatRoom" element={<ChatRoom username="admin" />} />
+        </Routes>
+      </BrowserRouter>
+
     </>
   );
 }
