1944aa5fec95e354c9e9369042289fab6a4d14d1
KubaDD <kuburolek11@gmail.com>
2024-02-10 20:16:03 +0100

    Chat room list table prototype added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import NavBar from "../components/NavBar";
+import { Container, Table } from "react-bootstrap";
 
 const Home = () => {
 
@@ -6,7 +7,35 @@ const Home = () => {
 
         <>
             <NavBar />
-            <h1>This is a home page</h1>
+            
+            <Container className="d-flex flex-wrap justify-content-center">
+                <div className="text-center w-100 mb-5">
+                    <button type="button" className="btn btn-primary mt-2">Create new chat room</button>
+                </div>
+
+                <Table className="border w-50 text-center">
+                    <thead>
+                        <tr>
+                            <th>Chat room id</th>
+                            <th>Join</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <td>
+                                test id
+                            </td>
+                            <td>
+                                <div className="d-inline-block">
+                                    <button className="btn btn-primary">test button</button>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </Table>
+
+            </Container>
 
         </>
 
