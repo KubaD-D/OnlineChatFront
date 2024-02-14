d0bcbf206eaf78a3f02f8956d5d05d0bee018fcd
KubaDD <kuburolek11@gmail.com>
2024-02-14 21:59:25 +0100

    Basic SignalR connection works

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -1,13 +1,46 @@
 import { useState, useEffect } from "react";
 import MessageContainer from "./MessageContainer";
 import ChatBoxFooter from "./ChatBoxFooter";
+import useFetch from "../utils/useFetch"
+import { HubConnectionBuilder, LogLevel } from "@microsoft/signalr";
 
 const ChatBox = ({ chatRoomId }) => {
+
+    const { data } = useFetch(`${process.env.REACT_APP_BACKEND_URL}/api/ChatRoom/${chatRoomId}/messages`, !chatRoomId);
+    const [connection, setConnection] = useState();
+
+    useEffect(() => {
+
+        joinRoom();
+
+    }, [chatRoomId])
+
+    const joinRoom = async () => {
+        try {
+
+            const connection = new HubConnectionBuilder()
+            .withUrl(`${process.env.REACT_APP_BACKEND_URL}/chat-room-hub`)
+            .configureLogging(LogLevel.Information)
+            .build();
+
+            connection.on("UserJoined", (username) => {
+                console.log(`Username joined a chat hub: ${username}`);
+            })
+
+            await connection.start();
+            await connection.invoke("JoinRoom", chatRoomId);
+
+            setConnection(connection);
+
+        } catch(err) {
+            console.error(err);
+        }
+    }
 
     return (
         <div className="chat-box col-md-8">
 
-            <MessageContainer chatRoomId={chatRoomId} />
+            <MessageContainer messages={data} />
             <ChatBoxFooter />
             
         </div>
--- src/components/MessageContainer.js
+++ src/components/MessageContainer.js
@@ -2,17 +2,15 @@ import { Container } from "react-bootstrap";
 import Message from "./Message";
 import useFetch from "../utils/useFetch";
 
-const MessageContainer = ({ chatRoomId }) => {
-
-    const { data } = useFetch(`${process.env.REACT_APP_BACKEND_URL}/api/ChatRoom/${chatRoomId}/messages`, !chatRoomId);
+const MessageContainer = ({ messages }) => {
 
     return (
         <div className="message-container overflow-auto d-flex flex-column justify-content-end" style={{ height: "85vh" }}>
-            {!data
+            {!messages
             ?
             <h1>Loading...</h1>
             :
-            data.map((message, index) => {
+            messages.map((message, index) => {
                 return (
                 <Message key={index} sender={message.sender} timeSent={message.timeSent} content={message.content} />
                 );
